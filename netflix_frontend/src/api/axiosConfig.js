4a716ee5e27020084eb5b71027eca7d31f30bea8
Daniel Onyeakazi <do4009@hw.ac.uk>
2026-09-11 15:48:33 +0100

    cleaned up frontend

--- netflix_frontend/src/api/axiosConfig.js
+++ netflix_frontend/src/api/axiosConfig.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:'http://18.221.67.127:8080',
+    baseURL: 'http://localhost:8080', // Replace with your EC2 IP when deployed (e.g. 'http://18.221.67.127:8080')
     headers: {
         'Content-Type': 'application/json',
-    },
+        'ngrok-skip-browser-warning': 'true'
+    }
 });
